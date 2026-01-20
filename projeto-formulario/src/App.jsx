74201f0659b2bbfdf7fda2661d880b28a1b0cbaa
Pedro <pedromsouza97@gmail.com>
2026-01-20 13:13:46 -0300

    Initial commit

---
+++ projeto-formulario/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaico para avaliar o produto</p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
